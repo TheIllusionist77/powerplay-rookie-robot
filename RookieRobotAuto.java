83df59ab1a07e88b18b2b772ba480c986da1e505
TheIllusionist77 <113849310+TheIllusionist77@users.noreply.github.com>
2023-03-19 18:10:13 -0400

    Update RookieRobotAuto.java

--- RookieRobotAuto.java
+++ RookieRobotAuto.java
@@ -9,11 +9,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class RookieRobotAuto extends LinearOpMode {
     DcMotor fl, bl, fr, br, left_slide, right_slide;
 
-    int fl_pos = 0;
-    int fr_pos = 0;
-    int bl_pos = 0;
-    int br_pos = 0;
-
     @Override
     public void runOpMode() throws InterruptedException {
         fl = hardwareMap.get(DcMotor.class, "front_left_motor");
@@ -25,6 +20,7 @@ public class RookieRobotAuto extends LinearOpMode {
 
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
+
         left_slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         right_slide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
@@ -33,56 +29,72 @@ public class RookieRobotAuto extends LinearOpMode {
 
         waitForStart();
         if (opModeIsActive()) {
-            moveMecanum(0, 6, 0, 0.5);
+            moveMecanum(12, 0, 0, 0.5);
+            Thread.sleep(2000);
+            moveMecanum(12, 0, 0, -0.5);
+            Thread.sleep(2000);
+            moveMecanum(0, 12, 0, 0.5);
+            Thread.sleep(2000);
+            moveMecanum(0, 12, 0, -0.5);
+            Thread.sleep(2000);
+            moveMecanum(0, 0, 12, 0.5);
+            Thread.sleep(2000);
+            moveMecanum(0, 0, 12, -0.5);
         }
     }
 
     void moveMecanum(double forward, double strafe, double rotate, double power) {
-        double power_constant = Math.max(Math.abs(forward), Math.abs(strafe));
-        double forwardp = (forward / power_constant) * power;
-        double strafep = (strafe / power_constant) * power;
+        int fl_init = fl.getCurrentPosition();
+        int fr_init = fr.getCurrentPosition();
+        int bl_init = bl.getCurrentPosition();
+        int br_init = br.getCurrentPosition();
 
-        double flp = (forwardp+strafep);
-        double blp = (forwardp-strafep);
-        double frp = (forwardp-strafep);
-        double brp = (forwardp+strafep);
+        int fl_pos = fl.getCurrentPosition();
+        int fr_pos = fr.getCurrentPosition();
+        int bl_pos = bl.getCurrentPosition();
+        int br_pos = br.getCurrentPosition();
 
-        double max = Math.max(Math.max(Math.abs((flp)), Math.abs(blp)), Math.max(Math.abs(frp), Math.abs(brp)));
-        if (max > 1) {
-            flp /= max;
-            blp /= max;
-            frp /= max;
-            brp /= max;
-        }
+        if (forward != 0) {
+            while (Math.abs(fl_pos - fl_init) < forward * 31 && Math.abs(fr_pos - fr_init) < forward * 31 && Math.abs(bl_pos - bl_init) < forward * 31 && Math.abs(br_pos - br_init) < forward * 31) {
+                fl.setPower(power);
+                bl.setPower(power);
+                fr.setPower(power);
+                br.setPower(power);
 
-        fl.setPower(flp);
-        bl.setPower(blp);
-        fr.setPower(frp);
-        br.setPower(brp);
+                fl_pos = fl.getCurrentPosition();
+                fr_pos = fr.getCurrentPosition();
+                bl_pos = bl.getCurrentPosition();
+                br_pos = br.getCurrentPosition();
+            }
+        } else if (strafe != 0) {
+            while (Math.abs(fl_pos - fl_init) < strafe * 62 && Math.abs(fr_pos - fr_init) < strafe * 62 && Math.abs(bl_pos - bl_init) < strafe * 62 && Math.abs(br_pos - br_init) < strafe * 62) {
+                fl.setPower(power);
+                bl.setPower(-power);
+                fr.setPower(-power);
+                br.setPower(power);
 
-        while (Math.abs(fl_pos) <= (forward + strafe) * 45 || Math.abs(fr_pos) <= (forward + strafe) * 45 || Math.abs(bl_pos) <= (forward + strafe) * 45|| Math.abs(br_pos) <= (forward + strafe) * 45) {
-            telemetry.addData("Motor", fl_pos);
-            telemetry.update();
+                fl_pos = fl.getCurrentPosition();
+                fr_pos = fr.getCurrentPosition();
+                bl_pos = bl.getCurrentPosition();
+                br_pos = br.getCurrentPosition();
+            }
+        } else if (rotate != 0) {
+            while (Math.abs(fl_pos - fl_init) < rotate * 124 && Math.abs(fr_pos - fr_init) < rotate * 124 && Math.abs(bl_pos - bl_init) < rotate * 124 && Math.abs(br_pos - br_init) < rotate * 124) {
+                fl.setPower(power);
+                bl.setPower(power);
+                fr.setPower(-power);
+                br.setPower(-power);
 
-            fl_pos = fl.getCurrentPosition();
-            fr_pos = fr.getCurrentPosition();
-            bl_pos = bl.getCurrentPosition();
-            br_pos = br.getCurrentPosition();
+                fl_pos = fl.getCurrentPosition();
+                fr_pos = fr.getCurrentPosition();
+                bl_pos = bl.getCurrentPosition();
+                br_pos = br.getCurrentPosition();
+            }
         }
 
         fl.setPower(0);
         bl.setPower(0);
         fr.setPower(0);
         br.setPower(0);
-
-        fl_pos = 0;
-        fr_pos = 0;
-        bl_pos = 0;
-        br_pos = 0;
-
-        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 }
